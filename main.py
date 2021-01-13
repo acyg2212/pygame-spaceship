1722d0924d3cb3a16dd7d0abbf7bda8338291c4c
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-01-13 13:16:39 -0500

    changed background to space

--- main.py
+++ main.py
@@ -31,12 +31,13 @@ RED_SPACESHIP_IMAGE = pygame.image.load(
 RED_SPACESHIP = pygame.transform.rotate(
     pygame.transform.scale(RED_SPACESHIP_IMAGE, (55, 40)), 270)
 
-SPACE = pygame.image.load(os.path.join('Assets', 'space.png'))
+SPACE = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
 
 
 def draw_window(red, yellow, red_bullets, yellow_bullets):
 
-    WIN.fill(WHITE)
+    WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
